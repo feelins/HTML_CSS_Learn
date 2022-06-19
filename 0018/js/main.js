760c9ef019479cf8e1370afa5aa8e810d0596b04
feelins <feipengshao@163.com>
2022-06-19 22:37:01 +0800

    0018-移动端页面布局基本完成

--- 0018/js/main.js
+++ 0018/js/main.js
@@ -1,16 +1,52 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
+// var swiper = new Swiper(".mySwiper", {
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+let swiperProducts = new Swiper(".products__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
 
-
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // 640: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20,
+        // },
+        // 768: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40,
+        // },
+        1024: {
+            // slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
